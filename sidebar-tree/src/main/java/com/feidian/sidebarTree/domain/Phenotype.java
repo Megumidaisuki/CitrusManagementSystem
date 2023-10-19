76bf30dfe18cb3b00b9b87c8f847ec8d8f35a5e4
Kannen <qkn0208@webmail.hzau.edu.cn>
2023-10-19 18:51:11 +0800

    fix:1.数据好像并没有失真；2.修改了PhenotypeFileMapper.xml的部分代码，使之适应新项目。

--- sidebar-tree/src/main/java/com/feidian/sidebarTree/domain/Phenotype.java
+++ sidebar-tree/src/main/java/com/feidian/sidebarTree/domain/Phenotype.java
@@ -18,53 +18,9 @@ public class Phenotype extends BaseEntity
     /** 自增主键 */
     private Long phenotypeId;
 
-    /** 物种ID */
-    @Excel(name = "物种ID")
-    private String speciesId;
-
-    /** 群体ID */
-    @Excel(name = "群体ID")
-    private Long populationId;
-
-    /** 数据采集年份 */
-    @Excel(name = "数据采集年份")
-    private String year;
-
-    /** 数据采集地区 */
-    @Excel(name = "数据采集地区")
-    private String location;
-
-    /** 重复实验 */
-    @Excel(name = "重复实验")
-    private Long repeat;
-
-    /** 品种ID */
-    @Excel(name = "品种ID")
-    private String kindId;
-
-    /** 品种名称 */
-    @Excel(name = "品种名称")
-    private String kindName;
-
     /** 材料编号 */
     @Excel(name = "材料编号")
     private String materialId;
-
-    /** 田间编号 */
-    @Excel(name = "田间编号")
-    private Long fieldId;
-
-    /** 对照类型 */
-    @Excel(name = "对照类型")
-    private String controlType;
-
-    /** 父本 */
-    @Excel(name = "父本")
-    private String father;
-
-    /** 母本 */
-    @Excel(name = "母本")
-    private String mother;
 
     /** 性状ID */
     @Excel(name = "性状ID")
@@ -74,158 +30,20 @@ public class Phenotype extends BaseEntity
     @Excel(name = "性状值")
     private Long traitValue;
 
-    public void setPhenotypeId(Long phenotypeId) 
-    {
+    public Phenotype() {
+    }
+
+    public Phenotype(Long phenotypeId, String materialId, Long traitId, Long traitValue) {
         this.phenotypeId = phenotypeId;
-    }
-
-    public Long getPhenotypeId() 
-    {
-        return phenotypeId;
-    }
-    public void setSpeciesId(String speciesId) 
-    {
-        this.speciesId = speciesId;
-    }
-
-    public String getSpeciesId() 
-    {
-        return speciesId;
-    }
-    public void setPopulationId(Long populationId) 
-    {
-        this.populationId = populationId;
-    }
-
-    public Long getPopulationId() 
-    {
-        return populationId;
-    }
-    public void setYear(String year) 
-    {
-        this.year = year;
-    }
-
-    public String getYear() 
-    {
-        return year;
-    }
-    public void setLocation(String location) 
-    {
-        this.location = location;
-    }
-
-    public String getLocation() 
-    {
-        return location;
-    }
-    public void setRepeat(Long repeat) 
-    {
-        this.repeat = repeat;
-    }
-
-    public Long getRepeat() 
-    {
-        return repeat;
-    }
-    public void setKindId(String kindId) 
-    {
-        this.kindId = kindId;
-    }
-
-    public String getKindId() 
-    {
-        return kindId;
-    }
-    public void setKindName(String kindName) 
-    {
-        this.kindName = kindName;
-    }
-
-    public String getKindName() 
-    {
-        return kindName;
-    }
-    public void setMaterialId(String materialId) 
-    {
         this.materialId = materialId;
-    }
-
-    public String getMaterialId() 
-    {
-        return materialId;
-    }
-    public void setFieldId(Long fieldId) 
-    {
-        this.fieldId = fieldId;
-    }
-
-    public Long getFieldId() 
-    {
-        return fieldId;
-    }
-    public void setControlType(String controlType) 
-    {
-        this.controlType = controlType;
-    }
-
-    public String getControlType() 
-    {
-        return controlType;
-    }
-    public void setFather(String father) 
-    {
-        this.father = father;
-    }
-
-    public String getFather() 
-    {
-        return father;
-    }
-    public void setMother(String mother) 
-    {
-        this.mother = mother;
-    }
-
-    public String getMother() 
-    {
-        return mother;
-    }
-    public void setTraitId(Long traitId) 
-    {
         this.traitId = traitId;
-    }
-
-    public Long getTraitId() 
-    {
-        return traitId;
-    }
-    public void setTraitValue(Long traitValue) 
-    {
         this.traitValue = traitValue;
-    }
-
-    public Long getTraitValue() 
-    {
-        return traitValue;
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
             .append("phenotypeId", getPhenotypeId())
-            .append("speciesId", getSpeciesId())
-            .append("populationId", getPopulationId())
-            .append("year", getYear())
-            .append("location", getLocation())
-            .append("repeat", getRepeat())
-            .append("kindId", getKindId())
-            .append("kindName", getKindName())
-            .append("materialId", getMaterialId())
-            .append("fieldId", getFieldId())
-            .append("controlType", getControlType())
-            .append("father", getFather())
-            .append("mother", getMother())
             .append("traitId", getTraitId())
             .append("traitValue", getTraitValue())
             .append("createBy", getCreateBy())
@@ -234,5 +52,69 @@ public class Phenotype extends BaseEntity
             .append("updateTime", getUpdateTime())
             .append("remark", getRemark())
             .toString();
+    }
+
+    /**
+     * 获取
+     * @return phenotypeId
+     */
+    public Long getPhenotypeId() {
+        return phenotypeId;
+    }
+
+    /**
+     * 设置
+     * @param phenotypeId
+     */
+    public void setPhenotypeId(Long phenotypeId) {
+        this.phenotypeId = phenotypeId;
+    }
+
+    /**
+     * 获取
+     * @return materialId
+     */
+    public String getMaterialId() {
+        return materialId;
+    }
+
+    /**
+     * 设置
+     * @param materialId
+     */
+    public void setMaterialId(String materialId) {
+        this.materialId = materialId;
+    }
+
+    /**
+     * 获取
+     * @return traitId
+     */
+    public Long getTraitId() {
+        return traitId;
+    }
+
+    /**
+     * 设置
+     * @param traitId
+     */
+    public void setTraitId(Long traitId) {
+        this.traitId = traitId;
+    }
+
+    /**
+     * 获取
+     * @return traitValue
+     */
+    public Long getTraitValue() {
+        return traitValue;
+    }
+
+    /**
+     * 设置
+     * @param traitValue
+     */
+    public void setTraitValue(Long traitValue) {
+        this.traitValue = traitValue;
     }
 }
